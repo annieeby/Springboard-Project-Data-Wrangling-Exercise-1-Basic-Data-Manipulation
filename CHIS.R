d39ca21aadead941817602f5137d7da050a2d2bb
annieeby <eby.annie@gmail.com>
2018-11-17 22:16:06 -0600

    Add files via upload

---
+++ CHIS.R
@@ -0,0 +1,34 @@
+# Assignment: 
+# https://campus.datacamp.com/courses/data-visualization-with-ggplot2-1/chapter-5-qplot-and-wrap-up?ex=8 
+# https://www.springboard.com/workshops/data-science/learn#/curriculum/792
+
+
+library(dplyr)
+library(ggplot2)
+
+f <- file.choose()
+titanic <- read.csv(f)
+#select titanic3.csv
+titanic
+
+# titanic is avaliable in your workspace
+# 1 - Check the structure of titanic
+str(titanic)
+
+# 2 - Use ggplot() for the first instruction
+ggplot(titanic, aes(x = Pclass, fill = Sex)) +
+  geom_bar(position = "dodge")
+
+# 3 - Plot 2, add facet_grid() layer
+ggplot(titanic, aes(x = Pclass, fill = Sex)) +
+  geom_bar(position = "dodge")+
+  facet_grid(. ~ Survived) 
+
+# 4 - Define an object for position jitterdodge, to use below
+posn.jd <- position_jitterdodge(0.5, 0, 0.6)
+
+# 5 - Plot 3, but use the position object from instruction 4
+ggplot(titanic, aes(x = Pclass, y = Age, col = Sex)) +
+  geom_point(size=3, alpha = 0.5, position = posn.jd)+
+  facet_grid(. ~ Survived) 
+
